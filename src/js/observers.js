09918563c31d8ef78d1d57b770eb3e70c98038ad
requena-sam <145543184+requena-sam@users.noreply.github.com>
2024-03-22 12:26:16 +0100

    Adding scrolling animation

---
+++ src/js/observers.js
@@ -0,0 +1,23 @@
+export const observers = {
+    showUpElements: document.querySelectorAll('[data-animation="showUp"]'),
+    init() {
+        this.showUpElements.forEach((element) => {
+            element.classList.add('opacity-none');
+        })
+        this.observersIntersection = new IntersectionObserver(this.showUpAdd)
+        this.observeAction();
+    },
+    showUpAdd(entries) {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('showUp');
+                entry.target.classList.remove('opacity-none')
+            }
+        })
+    },
+    observeAction() {
+        this.showUpElements.forEach((element) => {
+            this.observersIntersection.observe(element);
+        })
+    },
+}
